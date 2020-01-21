ad83a693766cbfe94ab16182377b7fd9855fff21
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-21 16:20:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,8 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
   result = []
   i = 0 
-  while i < movies_collection[i]
+  while i < movies_collection.length do 
+    
   
 end
 
